2d7670ce0a938658350c133a3fcca87a4f4aa00e
Maboey <manuel.martinbalmori@gmail.com>
2023-11-01 22:45:10 +0100

    animation jump/crouch and demo level
    added animations jump and crouch to the main character. created a demo level to test new gameplay

--- PlatformerNicoManu/Assets/scripts/Player.cs
+++ PlatformerNicoManu/Assets/scripts/Player.cs
@@ -14,11 +14,15 @@ public class Player : MonoBehaviour
     private CharacterController characterController;
     private Vector3 movementDirection = Vector3.zero;
     private Animator animator;
+    private bool iscrouching;
+    private float standingHeight, crouchHeight;
 
     private void Start()
     {
         characterController = this.gameObject.GetComponent<CharacterController>();
         animator = this.gameObject.GetComponent<Animator>();
+        standingHeight = characterController.height;
+        crouchHeight = standingHeight / 2;
     }
     void Update()
     {
@@ -29,6 +33,7 @@ public class Player : MonoBehaviour
     {
         
         bool playerGrounded = characterController.isGrounded;
+        animator.SetBool("grounded", playerGrounded);
 
         //if the player is running we set the speed acordingly
         float runningSpeed;
@@ -44,12 +49,8 @@ public class Player : MonoBehaviour
         //we get the direction at wich the player is trying to go (and the magnitude at wich it tries to go)
         Vector3 cameraDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         float inputMagnitude = Mathf.Clamp01(cameraDirection.magnitude);
-
-
-
         cameraDirection = Quaternion.AngleAxis(playerCamTransform.rotation.eulerAngles.y, Vector3.up) * cameraDirection;
         cameraDirection.Normalize();
-
 
         //if the player stops trying to run we save it
         if (inputMagnitude <= 0.1f && inputMagnitude >= -0.1f)
@@ -67,17 +68,34 @@ public class Player : MonoBehaviour
 
         characterController.Move(inputMovement * Time.deltaTime);
 
-
         //jumping
         if (Input.GetButtonDown("Jump") && playerGrounded)
         {
             doubleJumped = false;
             movementDirection.y = jumpSpeed;
+            animator.Play("sauter");
         }
         else if (Input.GetButtonDown("Jump") && (doubleJumped == false))
         {
             movementDirection.y = jumpSpeed * 1.2f;
             doubleJumped = true;
+            animator.Play("doubleSaut");
+        }
+
+        //crouching
+        if(Input.GetButton("crouch") && playerGrounded && !playerRunning)
+        {
+            iscrouching = true;
+            characterController.height = crouchHeight;
+            characterController.center = new Vector3(0,crouchHeight/2,0);
+            animator.SetBool("crouch", true);
+        }
+        else
+        {
+            iscrouching = false;
+            characterController.height = standingHeight;
+            characterController.center = new Vector3(0, standingHeight / 2, 0);
+            animator.SetBool("crouch", false);
         }
 
         movementDirection.y -= gravity * Time.deltaTime;
